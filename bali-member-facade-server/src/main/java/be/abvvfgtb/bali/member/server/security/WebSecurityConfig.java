9d0769c39bedfdf4aaa2ee7a25a0a9f47e19d1fa
dhiaeddineakoubi@hotmail.com <Taraji2011.*>
2022-09-07 13:34:22 +0200

    'security'

---
+++ bali-member-facade-server/src/main/java/be/abvvfgtb/bali/member/server/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package be.abvvfgtb.bali.member.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Web Security.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeHttpRequests((requests) -> requests
+                    .antMatchers(HttpMethod.GET, "/v1/**")
+                    .permitAll()
+                    .anyRequest().authenticated()
+            );
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user =
+            User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("admin")
+                    .roles("ADMIN")
+                    .build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+}
